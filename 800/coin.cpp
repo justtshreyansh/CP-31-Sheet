bdb14b321dedac9c2cb830eabf452b26eff62194
Shreyansh <shreyanshmarshal2@gmail.com>
2024-01-22 13:52:46 +0530

    Sorting questions based on ratings

---
+++ 800/coin.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n,k;
+        cin>>n>>k;
+        if(n%2==0 || n%k==0){
+            cout<<"YES"<<endl;
+        }
+        else if()
+
+    }
+    return 0;
+}
